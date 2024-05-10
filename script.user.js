4ceeb4a19bb976260fa71c5cb571491293fd9603
RedHandsome128 <159307358+RedHandsome128@users.noreply.github.com>
2024-05-10 22:57:01 +0300

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -13,6 +13,7 @@
 // @exclude *://*.reddit.com/media*
 // @exclude  *://*.reddit.com/mod/*/insights
 // @exclude *://*.reddit.com/mod/*/automations
+// @exclude *://*.reddit.com/mod/*/log
 // @exclude *://*.reddit.com/login*
 // @run-at document-start
 // @grant        none
